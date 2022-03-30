1d78294e449b381f4228369196a88bf56ecab513
Matt S <mschwell@nearix.com>
2022-03-30 11:13:33 -0500

    Adding in drop-down

--- src/index.js
+++ src/index.js
@@ -3,15 +3,22 @@ import React, { Component } from "react";
 import { render } from "react-dom";
 import { GithubPicker   } from 'react-color';
 
-import { Stage, Layer, Rect } from "react-konva";
+import { Stage, Layer, Rect, Circle } from "react-konva";
 import Konva from 'konva';
 
 class App extends Component {
+
   state = {
     isDragging: false,
     x: (window.innerWidth * .3),
     y: (window.innerHeight * .3),
-    color:"red"
+    color:"red",
+    shape:"rect",    
+    options : [
+      { label: '', value: 'Pick A Shape' },
+      { label: 'rect', value: 'Rectangual' },
+      { label: 'circle', value: 'Circle' },
+    ]
   };
 
   startX =(window.innerWidth * .3);
@@ -21,45 +28,85 @@ class App extends Component {
     this.setState({ color: color.hex });
   };
 
+  handleChangeShape = (event) => {
+    this.setState({ shape: event.target.value });
+  };
+
   render() {
+    const renderShape = () => {  
+      if (this.state.shape === "rect") {
+      return <Rect
+        width={100}
+        height={100}
+        fill={this.state.color}
+        shadowBlur={10}
+        x={this.state.x}
+        y={this.state.y}
+        draggable
+        onDragStart={() => {
+          this.setState({
+            isDragging: true,
+            color: Konva.Util.getRandomColor()
+          });
+        }}
+        onDragEnd={(e) => {
+          if(((this.startX - e.target.x()) < 30) &&  ((this.startY - e.target.y()) < 30)) {
+            this.setState({
+              color:"red"
+            });
+          }
+          this.setState({
+            isDragging: false,
+            x: e.target.x(),
+            y: e.target.y()
+          });
+        }}
+      />
+    } else {
+      return <Circle
+        width={100}
+        height={100}
+        fill={this.state.color}
+        x={this.state.x}
+        y={this.state.y}
+        draggable
+        onDragStart={() => {
+          this.setState({
+            isDragging: true,
+            color: Konva.Util.getRandomColor()
+          });
+        }}
+        onDragEnd={(e) => {
+          if(((this.startX - e.target.x()) < 30) &&  ((this.startY - e.target.y()) < 30)) {
+            this.setState({
+              color:"red"
+            });
+          }
+          this.setState({
+            isDragging: false,
+            x: e.target.x(),
+            y: e.target.y()
+          });
+        }}
+      />
+    }}
     return (
       <div className="container">
         <div className='row'>
         <div className='col-sm-10'>
-        <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Stage width={(window.innerWidth - 1000)} height={window.innerHeight}>
           <Layer>
-            <Rect
-              width={100}
-              height={100}
-              fill={this.state.color}
-              shadowBlur={10}
-              x={this.state.x}
-              y={this.state.y}
-              draggable
-              onDragStart={() => {
-                this.state.color=Konva.Util.getRandomColor()
-                this.setState({
-                  isDragging: true
-                });
-              }}
-              onDragEnd={(e) => {
-                if(((this.startX - e.target.x()) < 30) &&  ((this.startY - e.target.y()) < 30)) {
-                  this.setState({
-                    color:"red"
-                  });
-                }
-                this.setState({
-                  isDragging: false,
-                  x: e.target.x(),
-                  y: e.target.y()
-                });
-              }}
-            />
+            { renderShape()  }
           </Layer>
         </Stage>
       </div>
         <div className='col-sm-2'>
         <GithubPicker onChangeComplete={ this.handleChangeComplete } />
+        <select onChange={this.handleChangeShape}>
+          <option value="">Pick A Shape</option>
+          <option value="rect">Rectangal</option>
+          <option value="circle">Circle</option>
+        </select>
       </div>
       </div>
       </div>
